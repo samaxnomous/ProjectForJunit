cf70315bab83e549c626f4066057fa0fdb8b7237
samaxnomous <30555114+samaxnomous@users.noreply.github.com>
2021-02-02 23:56:27 +0530

    Update AccountService.java
    added comments from GitHub.com

--- src/com/bank/service/AccountService.java
+++ src/com/bank/service/AccountService.java
@@ -1,5 +1,5 @@
 package com.bank.service;
-
+//comment added from github.com for conflicts
 import com.bank.dal.AccountDAO;
 import com.bank.dal.AccountDAOInMemoryImpl;
 import com.bank.domain.Account;
